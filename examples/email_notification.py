df9b80fccc57045e1cb008e52b0073248699ff6b
Mike Cao <mike.cao@oracle.com>
2018-11-14 01:53:09 +0000

    put email password to internal config file
    Signed-off-by: Mike Cao <mike.cao@oracle.com>

--- examples/email_notification.py
+++ examples/email_notification.py
@@ -1,31 +1,37 @@
 import smtplib
+import oci
 from email.mime.text import MIMEText
 
 class Email:
     def __init__(self):
         self.mailto_list=["mike.cao@oracle.com"]
-        self.mail_host="smtp.126.com"
-        self.mail_user="vipmike007"
+        #self.mail_host="smtp.126.com"
+        self.mail_host="smtp.gmail.com"
+        self.mail_user="vipmike007@gmail.com"
         self.sender = "Mike Cao"
-        self.mail_pass="oracleS1"
-        self.mail_postfix="126.com"
+        config = oci.config.from_file()
+        self.mail_pass = config["email_password"]
+        self.mail_postfix="gmail.com.com"
 ######################
     def send_mail(self,sub,content):
         '''
         to_list:
         sub:
         content:
-        send_mail("aaa@126.com","sub","content")
+        send_mail("aaa@gmail.com","sub","content")
         '''
         to_list = self.mailto_list
-        me=self.sender+"<"+self.mail_user+"@"+self.mail_postfix+">"
+        me = "Mike Cao <mike.cao@oracle.com>"
         msg = MIMEText(content)
         msg['Subject'] = sub
         msg['From'] = me
         msg['To'] = ";".join(to_list)
         try:
-            s = smtplib.SMTP()
-            s.connect(self.mail_host)
+            s = smtplib.SMTP("smtp.gmail.com", 587)
+            #s.set_debuglevel(1)
+            s.ehlo()
+            s.starttls()
+            #s.connect(self.mail_host)
             s.login(self.mail_user,self.mail_pass)
             s.sendmail(me, to_list, msg.as_string())
             s.close()
