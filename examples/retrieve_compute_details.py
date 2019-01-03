721ec4ec2a9e53c9f0d5716f0bec1026f23e4fdb
Mike Cao <mike.cao@oracle.com>
2019-01-03 09:12:54 +0000

    New feature Added: do simple analysis before sending email
    Signed-off-by: Mike Cao <mike.cao@oracle.com>

--- examples/retrieve_compute_details.py
+++ examples/retrieve_compute_details.py
@@ -110,19 +110,33 @@ def get_all_volumes(volume, compartment_ocids):
             volume_monitor_summary = volume_handling(data, volume_monitor_summary)
         volume_summary_report.append(volume_monitor_summary)
     volume_monitor_summary_all = monitor_instance_per_c()
-    content = "================Volume Summry Report Per Comparment=======================\n"
+    content = ["","","",0]
+    content[0] = "================= Volume Block Device Usage Report========================\n"
+    content[2] = "================Volume Summry Report Per Comparment=======================\n"
     for i in range(len(volume_summary_report)):
         volume_monitor_summary_all.instances_count += volume_summary_report[i].instances_count
         volume_monitor_summary_all.instances_mem += volume_summary_report[i].instances_mem
-        content += "*Compartment_name \t\t%s\n" % (volume_summary_report[i].oc.name)
-        content += "*Compartment DESC \t\t%s\n" % (volume_summary_report[i].oc.description)
-        content += "*Total volume count\t\t%d\n" % (volume_summary_report[i].instances_count)
-        content += "*Total Volume Size:\t\t%dGB\n\n" % (volume_summary_report[i].instances_mem)
+        content[2] += "*Compartment_name \t\t%s\n" % (volume_summary_report[i].oc.name)
+        content[2] += "*Compartment DESC \t\t%s\n" % (volume_summary_report[i].oc.description)
+        content[2] += "*Total volume count\t\t%d\n" % (volume_summary_report[i].instances_count)
+        content[2] += "*Total Volume Size:\t\t%dGB\n\n" % (volume_summary_report[i].instances_mem)
   
-    content += "================Volume Summry Report in total=======================\n"
-    content += "*Total volume count\t\t%d\n" % (volume_monitor_summary_all.instances_count)
-    content += "*Total Volume Size\t\t%dGB\n" % (volume_monitor_summary_all.instances_mem)
-
+    content[1] += "================Volume Summry Report in total=======================\n"
+    content[1] += "*Total volume count\t\t%d\n" % (volume_monitor_summary_all.instances_count)
+    content[1] += "*Total Volume Size\t\t%dGB\n" % (volume_monitor_summary_all.instances_mem)
+    total_size = volume_monitor_summary_all.instances_mem*1024
+    if total_size == 0:
+        content[3] = 1
+    elif total_size >0 and total_size <= 700:
+        content[3] = 2
+    elif total_size > 700 and total_size <= 900:
+        content[3] = 3
+    elif total_size > 900 and total_size <=1024:
+        content[3] = 4
+    elif total_size > 1024:
+        content[3] = 5
+    else:
+        content[3] = 0
 
         #  Results for a compartment 'c' for a region defined
         #  in 'audit' object.
@@ -175,8 +189,9 @@ def get_all_instances(compute, compartment_ocids):
             instance_monitor_summary = instance_handling(data, instance_monitor_summary)
         instance_summary_report.append(instance_monitor_summary)
     instance_monitor_summary_all = monitor_instance_per_c()
-    content = ""
-    content += "================Instance Summry Report Per Comparment========================\n"
+    content = ["", "", "", 0]
+    content[0] = "================Instance Summry Report ========================\n"
+    content[2] = "================VM instances usage per compartment ===============\n"
     for i in range(len(instance_summary_report)):
         instance_monitor_summary_all.instances_count += instance_summary_report[i].instances_count
         instance_monitor_summary_all.instances_cpu += instance_summary_report[i].instances_cpu
@@ -187,26 +202,39 @@ def get_all_instances(compute, compartment_ocids):
         
         ##print results
         if instance_summary_report[i].running_instances_count !=0:
-            content += "*Compartment_name \t\t%s\n" % (instance_summary_report[i].oc.name)
-            content += "*Compartment DESC \t\t%s\n" % (instance_summary_report[i].oc.description)
-            content += "*Total instances count:\t\t%d\n" % (instance_summary_report[i].instances_count)
-            content += "*Running instances count:\t%d\n" % (instance_summary_report[i].running_instances_count)
-            content += "*Total instances CPU count:\t%d\n" % (instance_summary_report[i].instances_cpu)
-            content += "*Total instances mem count:\t%d\n" % (instance_summary_report[i].instances_mem)
-            content += "*Running instances CPU count:\t%d\n" % (instance_summary_report[i].running_instances_cpu)
-            content += "*Running instances mem count:\t%d GB \n\n" % (instance_summary_report[i].running_instances_mem)
+            content[2] += "*Compartment_name \t\t%s\n" % (instance_summary_report[i].oc.name)
+            content[2] += "*Compartment DESC \t\t%s\n" % (instance_summary_report[i].oc.description)
+            content[2]+= "*Total instances count:\t\t%d\n" % (instance_summary_report[i].instances_count)
+            content[2] += "*Running instances count:\t%d\n" % (instance_summary_report[i].running_instances_count)
+            content[2] += "*Total instances CPU count:\t%d\n" % (instance_summary_report[i].instances_cpu)
+            content[2] += "*Total instances mem count:\t%d\n" % (instance_summary_report[i].instances_mem)
+            content[2] += "*Running instances CPU count:\t%d\n" % (instance_summary_report[i].running_instances_cpu)
+            content[2] += "*Running instances mem count:\t%d GB \n\n" % (instance_summary_report[i].running_instances_mem)
         else:
-            content += "*Compartment %s:\t 0|%d instances running\n\n" % (instance_summary_report[i].oc.name, instance_summary_report[i].instances_count)
-        content += "**************************************************************************\n"
+            content[2] += "*Compartment %s:\t 0|%d instances running\n\n" % (instance_summary_report[i].oc.name, instance_summary_report[i].instances_count)
+        content[2] += "**************************************************************************\n"
 
     
-    content += "================Instance Summry Report All =================================\n"
-    content += "*Total instances count:\t%d\n" % (instance_monitor_summary_all.instances_count)
-    content += "*Total instances CPU count:\t%d\n" % (instance_monitor_summary_all.instances_cpu)
-    content += "*Total instances mem count:\t%d\n" % (instance_monitor_summary_all.instances_mem)
-    content += "*Running instances count:\t%d\n"% (instance_monitor_summary_all.running_instances_count)
-    content += "*Running instances CPU count:\t%d\n" % (instance_monitor_summary_all.running_instances_cpu)
-    content += "*Running instances mem count:\t%d GB\n" % (instance_monitor_summary_all.running_instances_mem)
+    content[1] += "================Instance Summry Report All =================================\n"
+    content[1] += "*Total instances count:\t%d\n" % (instance_monitor_summary_all.instances_count)
+    content[1] += "*Total instances CPU count:\t%d\n" % (instance_monitor_summary_all.instances_cpu)
+    content[1] += "*Total instances mem count:\t%d\n" % (instance_monitor_summary_all.instances_mem)
+    content[1] += "*Running instances count:\t%d\n"% (instance_monitor_summary_all.running_instances_count)
+    content[1] += "*Running instances CPU count:\t%d\n" % (instance_monitor_summary_all.running_instances_cpu)
+    content[1] += "*Running instances mem count:\t%d GB\n" % (instance_monitor_summary_all.running_instances_mem)
+    # judge if everything works fine
+    if instance_monitor_summary_all.instances_count  == 0:
+        content[3] = 1
+    elif instance_monitor_summary_all.instances_count > 0 and instance_monitor_summary_all.instances_count <=8:
+        content[3] = 2
+    elif instance_monitor_summary_all.instances_count > 8 and instance_monitor_summary_all.instances_count <=10:
+        content[3] = 3
+    elif instance_monitor_summary_all.instances_count > 10 and instance_monitor_summary_all.instances_count <=12:
+        content[3] = 4
+    elif instance_monitor_summary_all.instances_count > 12:
+        content[3] = 5
+    else:
+        content[3] = 0
     return content
 
 def get_all_db_system(dbs, compartment_ocids):
@@ -217,12 +245,12 @@ def get_all_db_system(dbs, compartment_ocids):
             dbs.list_db_systems,
             compartment_id=c).data
         list_of_db_systems.extend(db_lists)
-    content = ""
+    content = ["", "", "", 0]
     count = 0
     size = 0
     cpu = 0
     special_monitoring = ""
-    content += "================Database Summry Report Per Comparment========================\n"
+    content[0] = "================ Database Summry Report ===========================\n"
     for i in list_of_db_systems:
         if i.lifecycle_state == "AVAILABLE" or i.lifecycle_state == "PROVISIONING":
             count += 1
@@ -230,10 +258,23 @@ def get_all_db_system(dbs, compartment_ocids):
             cpu += get_cpu_by_shape(i.shape)
             if i.database_edition != "STANDARD_EDITION":
                 special_monitoring += "DB %s are using %s DB\n" %(i.display_name,i.database_edition)
-    content += "*Total DB count is %d \n" %(count)
-    content += "*Total DB cpu is %d \n" %(cpu)
-    content += "*Total DB extra storage size is %d \n" % (size)
-    content += special_monitoring
+    content[1] += "*Total DB count is %d \n" %(count)
+    content[1] += "*Total DB cpu is %d \n" %(cpu)
+    content[1] += "*Total DB extra storage size is %d \n" % (size)
+    content[1] += special_monitoring
+
+    if count == 0:
+        content[3] = 1
+    elif count > 0 and count <=2:
+        content[3] = 2
+    elif count > 2 and count <=3:
+        content [3] = 3
+    elif count > 3 and count <=4:
+        content [3] = 4
+    elif count >4 :
+        content[3] = 5
+    else:
+        content[3] = 0
     
 #    content = instance_handling(list_of_db_systems)
     return content
@@ -245,8 +286,9 @@ def get_all_buckets(object_storage_client, comparmments):
     return object_buckets_list
 
 def get_all_object_size(object_storage_client, buckets):
-    content =  "  ================OBJECT Summry Report ========================\n"
-    content += "*total object size: \t "
+    content = ["","","", 0]
+    content[0] =  "  ================OBJECT Summry Report ========================\n"
+    content[1] = "*total object size: \t "
     namespace = object_storage_client.get_namespace().data
     object_list = []
     total_size = 0.000000000000000000000000
@@ -255,8 +297,58 @@ def get_all_object_size(object_storage_client, buckets):
         object_list.extend(o_list)
     for i in object_list:
         total_size += i.size/1048576.000000000
-    content = content + str(total_size) + " MB\n"
+    content[1] = content[1] + str(total_size) + " MB\n"
+
+    total_size = total_size/1024.000000000
+    
+    if total_size == 0:
+        content[3] = 1
+    elif total_size >0 and total_size <= 700:
+        content[3] = 2
+    elif total_size > 700 and total_size <= 900:
+        content[3] = 3
+    elif total_size > 900 and total_size <=1024:
+        content[3] = 4
+    elif total_size > 1024:
+        content[3] = 5
+    else:
+        content[3] = 0
+
     return content
+
+def check_storage_summary(nfs_storage_client, compartments, identity):
+    #get list of availability domain first
+    size = 0
+    content = ["", "", "", 0]
+    content[0] =  "  ================FSS(nfs) Summry Report ========================\n"
+    content[1] += "*total File System service size: \t "
+
+    for c in compartments:
+        a_domain_s = identity.list_availability_domains(c.id).data
+        for a in a_domain_s:
+            summary_list = nfs_storage_client.list_file_systems(c.id, a.name).data
+            if len(summary_list) != 0:
+                for j in summary_list:
+                    size += j.metered_bytes
+
+    total_size = size / 1048576.000000000/ 1024.00000
+    content[1] = content[1] + str(total_size) + " GB\n"
+    if total_size ==0:
+        content[3] = 1
+    elif total_size >0 and total_size <= 700:
+        content[3] = 2
+    elif total_size > 700 and total_size <= 900:
+        content[3] = 3
+    elif total_size > 900 and total_size <=1024:
+        content[3] = 4
+    elif total_size > 1024:
+        content[3] = 5
+    else:
+        content[3] = 0
+
+
+    return content
+
 #  Setting configuration
 #  Default path for configuration file is "~/.oci/config"
 #config = oci.config.from_file()
@@ -266,29 +358,13 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 2:
     config = oci.config.from_file(file_location='~/.oci/config', profile_name = sys.argv[1])
     tenancy_name = sys.argv[1]
+elif len(sys.argv) == 3:
+    config = oci.config.from_file(file_location='~/.oci/config', profile_name = sys.argv[1])
+    tenancy_name = sys.argv[1]
+    email_sent = False
 else:
     print 'print error found'
     sys.exit(1)
-
-def check_storage_summary(nfs_storage_client, compartments, identity):
-    #get list of availability domain first
-    size = 0
-    content =  "  ================OBJECT Summry Report ========================\n"
-    content += "*total File System service size: \t "
-
-    for c in compartments:
-        a_domain_s = identity.list_availability_domains(c.id).data
-        for a in a_domain_s:
-            summary_list = nfs_storage_client.list_file_systems(c.id, a.name).data
-            if len(summary_list) != 0:
-                for j in summary_list:
-                    size += j.metered_bytes
-    
-    total_size = size / 1048576.000000000
-    content = content + str(total_size) + " MB\n"
-
-    return content
-
 tenancy_id = config["tenancy"]
 
 #  Initiate the client with the locally available config.
@@ -300,35 +376,63 @@ regions = get_regions(identity)
 # This array will be used to store the list of compartments in the tenancy.
 compartments = get_compartments(identity, tenancy_id)
 
-
+DS_status={1 :'No Use - All Good' , 
+           2: 'in Use - All Good', 
+           3: 'Warning - Need review' , 
+           4: 'Fatal alerts - Need review', 
+           5: 'OverLoaded - Need Action ASAP',
+           0: 'Scripts Error - Contact Mike.Cao to check the RCA'}
 compute_client = oci.core.ComputeClient(config)
 shape = oci.core.models.Shape()
 block_storage_client = oci.core.BlockstorageClient(config)
 db_client = oci.database.DatabaseClient(config)
+object_storage_client = oci.object_storage.ObjectStorageClient(config)
+nfs_storage_client = oci.file_storage.FileStorageClient(config)
+db_client.base_client.set_region('us-ashburn-1')
+
+to_list=(str(config["to_list"])).split(',')
+cc_list=(str(config["cc_list"])).split(',')
+
 
 #  For each region get the logs for each compartment.
 compute_client.base_client.set_region('us-ashburn-1')
+content = ""
+report_status_id = 0
 instance_content = get_all_instances(compute_client, compartments)
-#send_report_out("Compute Audit Report -"+datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M"),content)
-#print get_all_shapes(compute_client,compartments)
-volume_content = get_all_volumes(block_storage_client, compartments)
-#send_report_out("Block Audit Report -"+datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M"),content)
-db_client.base_client.set_region('us-ashburn-1')
+if instance_content[3] > report_status_id:
+    report_status_id = instance_content[3]
+
+content = content + instance_content[0] + instance_content[1]
+if instance_content[3] != 1 and instance_content[3] != 2:
+    content = content + instance_content[2]
+
 db_content = get_all_db_system(db_client,compartments)
-#content = instance_content + volume_content + db_content
-to_list=(str(config["to_list"])).split(',')
-cc_list=(str(config["cc_list"])).split(',')
-#send_report_out("tenancy - " + tenancy_name + "-"+datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M"),content,to_list,cc_list)
+if db_content[3] > report_status_id:
+    report_status_id = db_content[3]
+## DB do not support check per compartments
+content = content + db_content[0] + db_content[1]
+
+
+volume_content = get_all_volumes(block_storage_client, compartments)
+if volume_content[3] > report_status_id:
+    report_status_id = volume_content[3]
+content = content + volume_content[0] + volume_content[1]
+if volume_content[3] != 1 and volume_content[3] != 2:
+    content = content + volume_content[2]
+
 ### support object 
-object_storage_client = oci.object_storage.ObjectStorageClient(config)
 buckets = get_all_buckets(object_storage_client, compartments)
 object_content = get_all_object_size(object_storage_client,  buckets)
-content = instance_content + volume_content + db_content + object_content
+if object_content[3] > report_status_id:
+    report_status_id = object_content[3]
+content = content + object_content[0] + object_content[1]
 
-nfs_storage_client = oci.file_storage.FileStorageClient(config)
 nfs_storage = check_storage_summary(nfs_storage_client, compartments, identity)
+if nfs_storage[3] > report_status_id:
+    report_status_id = nfs_storage[3]
+content = content + nfs_storage[0] + nfs_storage[1]
 
-content = instance_content + db_content + volume_content + object_content + nfs_storage
+print DS_status[report_status_id]
 print content
-send_report_out("tenancy - " + tenancy_name + "-"+datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M"),content,to_list,cc_list)
+send_report_out("Tenancy - " + tenancy_name + "-"+datetime.datetime.utcnow().strftime("%Y-%m-%d-%H:%M") + ' - ' + DS_status[report_status_id],content,to_list,cc_list)
 
